8b92eee8f771a28394c6903eae8fa6871d482a38
Alexey Yegorov <ye.alexey@gmail.com>
2022-05-19 18:50:02 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const handle: React.FormEventHandler<HTMLFormElement> = async (event) => {
+    try {
+      event.preventDefault();
+      const response = await axios({
+        method: 'post',
+        url: 'http://localhost:4000/auth/signin',
+        data: {
+          email: '',
+          password: '',
+        },
+      });
+      console.log('response >>>', response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handle}>
+      <label htmlFor="email">Email:</label>
+      <input type="text" name="email" id="email" placeholder="email" />
+      <label htmlFor="password">Password: </label>
+      <input type="password" name="password" id="password" placeholder="password"/>
+      <button type="submit">SUBMIT</button>
+    </form>
   );
 }
 
