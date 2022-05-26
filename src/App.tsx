4a69dc60a9e4c1b37beed3f6dff4d90901c8bec0
Alexey Yegorov <ye.alexey@gmail.com>
2022-05-26 17:36:11 +0300

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 
 import Header from './ui/containers/Header/Header';
 import Footer from './ui/containers/Footer';
-import MainWrapper from './ui/styles/MainWrapper';
 import Navigation from './ui/containers/Navigation';
 
 const App = () => {
   return (
     <>
       <Header />
-      <MainWrapper>
         <Navigation />
-      </MainWrapper>
       <Footer />
     </>
   );
--- src/ui/containers/Navigation.tsx
+++ src/ui/containers/Navigation.tsx
@@ -5,15 +5,18 @@ import SignUpForm from '../pages/Auth/SignUp';
 import Home from '../pages/Home/Home';
 import UserProfile from '../pages/UserProfile/UserProfile';
 import { routePath } from '../../constants';
+import MainWrapper from '../styles/MainWrapper';
 
 const Navigation = () => {
   return (
-    <Routes>
-      <Route path={routePath.home} element={< Home />} />
-      < Route path={routePath.signIn} element={< SignInForm />} />
-      < Route path={routePath.signUp} element={< SignUpForm />} />
-      < Route path={routePath.profile} element={< UserProfile />} />
-    </Routes>
+    <MainWrapper>
+      <Routes>
+        <Route path={routePath.home} element={< Home />} />
+        < Route path={routePath.signIn} element={< SignInForm />} />
+        < Route path={routePath.signUp} element={< SignUpForm />} />
+        < Route path={routePath.profile} element={< UserProfile />} />
+      </Routes>
+    </MainWrapper>
   );
 };
 
